ab8c86b83c1ee556e20ca7ec091d0f7fe87bc332
unknown <shubhamtanwar97@gmail.com>
2018-11-05 02:31:03 +0530

    first look at confined search and path matching

---
+++ fast_corners.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import sys
+import os
+
+def import_image(path):
+    pass
+
+def find_corner_points(image_object):
+    return_list_of_coordinates = []
+    return return_list_of_coordinates
+
+def find_r_theta(point_coordinate,centre_coordinate):
+    r = 0;
+    o = 0;
+    return(r,o)
+def vote():
+    # initiate vote matrix
+    # for all points find the centre and then find the theta and the r
+    
+    pass
